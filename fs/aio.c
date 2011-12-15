2f045bb40b04dbfa1efc67eb57a499b7840a2802
Dave Kleikamp <dave.kleikamp@oracle.com>
2011-12-15 15:44:45 -0600

    AIO: Don't plug the I/O queue in do_io_submit()
    Asynchronous I/O latency to a solid-state disk greatly increased between the 2.6.32 and 3.0 kernels.
    By removing the plug from do_io_submit(), we observed a 34% improvement in the I/O latency.
    Unfortunately, at this level, we don't know if the request is to
    a rotating disk or not.
    
    Change-Id: I7101df956473ed9fd5dcff18e473dd93b688a5c1
    Signed-off-by: Dave Kleikamp <dave.kleikamp@oracle.com>
    Cc: linux-aio@kvack.org
    Cc: Chris Mason <chris.mason@oracle.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>
    Signed-off-by: GenZSouL <priyanshusinghal0818x@gmail.com>

--- fs/aio.c
+++ fs/aio.c
@@ -1654,7 +1654,6 @@ static long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i = 0;
-	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1670,8 +1669,6 @@ static long do_io_submit(aio_context_t ctx_id, long nr,
 		pr_debug("EINVAL: invalid context id\n");
 		return -EINVAL;
 	}
-
-	blk_start_plug(&plug);
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1695,7 +1692,6 @@ static long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
-	blk_finish_plug(&plug);
 
 	percpu_ref_put(&ctx->users);
 	return i ? i : ret;
