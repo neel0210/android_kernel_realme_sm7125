2631b9899a9a528a7706dce3aeb09bf5c0229fd1
Cyber Knight <cyberknight755@gmail.com>
2022-08-04 16:59:31 +0000

    drm/atomic: Boost CPU & DDR bus according to set kernel profile
    - Boosting is done only when KProfiles is set to mode 2 or 3.
    - Otherwise boosting is skipped.
    - This patch is adapted to the latest implementation of KProfiles.
    
    [@pri0818]
    -Adapt to our usage
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>
    Signed-off-by: priiii0818 <priyanshusinghal0818@gmail.com>
    Signed-off-by: GenZSouL <priyanshusinghal0818x@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -30,6 +30,7 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 #include <linux/sync_file.h>
 
@@ -2205,6 +2206,8 @@ static void complete_crtc_signaling(struct drm_device *dev,
 	kfree(fence_state);
 }
 
+extern int kp_active_mode(void);
+
 int drm_mode_atomic_ioctl(struct drm_device *dev,
 			  void *data, struct drm_file *file_priv)
 {
@@ -2249,9 +2252,21 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 		return -EINVAL;
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
-		devfreq_boost_kick(DEVFREQ_CPU_CPU_LLC_BW);
-	}
+	  /*
+	   * Dont boost CPU & DDR if battery saver profile is enabled
+	   * and boost CPU & DDR if balanced profile is enabled
+	   */
+	  if (kp_active_mode() == 3 || kp_active_mode() == 0) {
+	    cpu_input_boost_kick_max(25);	  
+	    devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 75);
+	    devfreq_boost_kick_max(DEVFREQ_CPU_CPU_LLC_BW, 75);
+	  } else if (kp_active_mode() == 2) {
+	    cpu_input_boost_kick_max(15);	  
+	    devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 20);
+	    devfreq_boost_kick_max(DEVFREQ_CPU_CPU_LLC_BW, 20);  		    
+      }	       
+}
+	  
 	drm_modeset_acquire_init(&ctx, 0);
 
 	state = drm_atomic_state_alloc(dev);
